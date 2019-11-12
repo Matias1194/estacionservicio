8839824c99570cbf159b10eecda14a35394e1b87
Matías <matiasmontiel94@hotmail.com>
2019-11-12 08:46:09 -0300

    Navegador actualizado con Dropdown para el admin. Módulos nuevos: Permisos, Reportes.

--- secciones/navegador.php
+++ secciones/navegador.php
@@ -10,54 +10,213 @@
     <div class="collapse navbar-collapse" id="navbarContenido">
         <?php if (isset($_SESSION['usuario'])) { ?>
         <ul class="navbar-nav mr-auto">
-            <?php if(tienePermiso(33)) { ?>
-            <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/proveedores.php' ? 'active' : '') ?>">
-                <a href="javascript:void(0)" class="nav-link" data-url="proveedores">
-                    Proveedores
+            
+            <!-- Usuarios -->
+            <?php if(tienePermiso(1) && tienePermiso(58)) { ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/usuarios.php' ? 'active' : '') ?>" href="javascript:void(0)" id="usuariosDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Usuarios
                 </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="usuariosDropdown">
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="usuarios">
+                        Playa
+                    </a>
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="usuarios">
+                        Mercado
+                    </a>
+                </div>
             </li>
-            <?php } if(tienePermiso(11)) { ?>
-            <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/clientes.php' ? 'active' : '') ?>">
-                <a href="javascript:void(0)" class="nav-link" data-url="clientes">
-                    Clientes
-                </a>
-            </li>
-            <?php } if(tienePermiso(1)) { ?>
+            <?php } else if(tienePermiso(1) || tienePermiso(58)) { ?>
             <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/usuarios.php' ? 'active' : '') ?>">
                 <a href="javascript:void(0)" class="nav-link" data-url="usuarios">
                     Usuarios
                 </a>
             </li>
-            <?php } if(tienePermiso(20)) { ?>
+
+            <!-- Permisos -->
+            <?php } if(tienePermiso(10) && tienePermiso(67)) { ?>
+            <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/permisos.php' ? 'active' : '') ?>">
+                <a href="javascript:void(0)" class="nav-link" data-url="permisos">
+                    Permisos
+                </a>
+            </li>
+
+            <!-- Clientes -->
+            <?php } if(tienePermiso(12) && tienePermiso(69)) { ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/clientes.php' ? 'active' : '') ?>" href="javascript:void(0)" id="clientesDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Clientes
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="clientesDropdown">
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="clientes">
+                        Playa
+                    </a>
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="clientes">
+                        Mercado
+                    </a>
+                </div>
+            </li>
+            <?php } else if(tienePermiso(12) || tienePermiso(69)) { ?>
+            <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/clientes.php' ? 'active' : '') ?>">
+                <a href="javascript:void(0)" class="nav-link" data-url="clientes">
+                    Clientes
+                </a>
+            </li>
+
+            <!-- Proveedores -->
+            <?php } if(tienePermiso(34) && tienePermiso(6991)) { ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/proveedores.php' ? 'active' : '') ?>" href="javascript:void(0)" id="proveedoresDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Proveedores
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="proveedoresDropdown">
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="proveedores">
+                        Playa
+                    </a>
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="proveedores">
+                        Mercado
+                    </a>
+                </div>
+            </li>
+            <?php } else if(tienePermiso(34) || tienePermiso(91)) { ?>
+            <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/proveedores.php' ? 'active' : '') ?>">
+                <a href="javascript:void(0)" class="nav-link" data-url="proveedores">
+                    Proveedores
+                </a>
+            </li>
+            
+            <!-- Compras -->
+            <?php } if(tienePermiso(21) && tienePermiso(78)) { ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/compras.php' ? 'active' : '') ?>" href="javascript:void(0)" id="comprasDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Compras
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="comprasDropdown">
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="compras">
+                        Playa
+                    </a>
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="compras">
+                        Mercado
+                    </a>
+                </div>
+            </li>
+            <?php } else if(tienePermiso(21) || tienePermiso(78)) { ?>
             <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/compras.php' ? 'active' : '') ?>">
                 <a href="javascript:void(0)" class="nav-link" data-url="compras">
                     Compras
                 </a>
             </li>
-            <?php } if(tienePermiso(43)) { ?>
+            
+            <!-- Ventas -->
+            <?php } if(tienePermiso(44) && tienePermiso(101)) { ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/ventas.php' ? 'active' : '') ?>" href="javascript:void(0)" id="ventasDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Ventas
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="ventasDropdown">
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="ventas">
+                        Playa
+                    </a>
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="ventas">
+                        Mercado
+                    </a>
+                </div>
+            </li>
+            <?php } else if(tienePermiso(44) || tienePermiso(101)) { ?>
             <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/ventas.php' ? 'active' : '') ?>">
                 <a href="javascript:void(0)" class="nav-link" data-url="ventas">
                     Ventas
                 </a>
             </li>
-            <?php } if(tienePermiso(27)) { ?>
+            
+            <!-- Productos -->
+            <?php } if(tienePermiso(28) && tienePermiso(85)) { ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/productos.php' ? 'active' : '') ?>" href="javascript:void(0)" id="productosDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Productos
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="productosDropdown">
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="productos">
+                        Playa
+                    </a>
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="productos">
+                        Mercado
+                    </a>
+                </div>
+            </li>
+            <?php } else if(tienePermiso(28) || tienePermiso(85)) { ?>
             <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/productos.php' ? 'active' : '') ?>">
                 <a href="javascript:void(0)" class="nav-link" data-url="productos">
                     Productos
                 </a>
             </li>
-            <?php } if(tienePermiso(42)) { ?>
+            
+            <!-- Stock -->
+            <?php } if(tienePermiso(43) && tienePermiso(100)) { ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/stock.php' ? 'active' : '') ?>" href="javascript:void(0)" id="stockDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Stock
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="stockDropdown">
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="stock">
+                        Playa
+                    </a>
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="stock">
+                        Mercado
+                    </a>
+                </div>
+            </li>
+            <?php } else if(tienePermiso(43) || tienePermiso(100)) { ?>
             <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/stock.php' ? 'active' : '') ?>">
                 <a href="javascript:void(0)" class="nav-link" data-url="stock">
                     Stock
                 </a>
             </li>
-            <?php } if(tienePermiso(10)) { ?>
-                <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/caja.php' ? 'active' : '') ?>">
+            
+            <!-- Caja -->
+            <?php } if(tienePermiso(11) && tienePermiso(68)) { ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/caja.php' ? 'active' : '') ?>" href="javascript:void(0)" id="cajaDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Caja
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="cajaDropdown">
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="caja">
+                        Playa
+                    </a>
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="caja">
+                        Mercado
+                    </a>
+                </div>
+            </li>
+            <?php } else if(tienePermiso(11) || tienePermiso(68)) { ?>
+            <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/caja.php' ? 'active' : '') ?>">
                 <a href="javascript:void(0)" class="nav-link" data-url="caja">
                     Caja
                 </a>
             </li>
+
+            <!-- Reportes -->
+            <?php } if(tienePermiso(57) && tienePermiso(114)) { ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/reportes.php' ? 'active' : '') ?>" href="javascript:void(0)" id="reportesDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Reportes
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="reportesDropdown">
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="reportes">
+                        Playa
+                    </a>
+                    <a href="javascript:void(0)" class="dropdown-item" data-url="reportes">
+                        Mercado
+                    </a>
+                </div>
+            </li>
+            <?php } else if(tienePermiso(57) || tienePermiso(114)) { ?>
+            <li class="nav-item <?php echo ($_SERVER['PHP_SELF'] == '/estacionservicio/reportes.php' ? 'active' : '') ?>">
+                <a href="javascript:void(0)" class="nav-link" data-url="reportes">
+                    Reportes
+                </a>
+            </li>
+            
             <?php } ?>
         </ul>
         <?php } ?>
